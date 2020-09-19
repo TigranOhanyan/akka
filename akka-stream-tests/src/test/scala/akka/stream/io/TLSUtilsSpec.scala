4c7f182af1f5e928ba67121a0bb4d0141c250a2e
economist <tigranohanyan777@gmail.com>
2020-09-20 03:12:11 +0400

    Client Authentication Need Test.

---
+++ akka-stream-tests/src/test/scala/akka/stream/io/TLSUtilsSpec.scala
@@ -0,0 +1,29 @@
+package akka.stream.io
+
+import akka.stream.TLSClientAuth
+import akka.stream.impl.io.TlsUtils
+import akka.stream.TLSProtocol.NegotiateNewSession
+import javax.net.ssl.{SSLContext, SSLEngine, SSLParameters}
+import org.scalatest.flatspec.AnyFlatSpec
+
+
+class TLSUtilsSpec extends AnyFlatSpec {
+
+  val sslEngine: SSLEngine = SSLContext.getDefault.createSSLEngine()
+  val sslParams: SSLParameters = new SSLParameters()
+  val negotiableSession: NegotiateNewSession = NegotiateNewSession(None,None,Some(TLSClientAuth.Need),Some(sslParams))
+
+  "SSLParameters" must "not by default require client authentication when it is not passed to the constructor of HttpConnectionContext. In that case its default value is used by akka-http." in {
+    assert(!sslParams.getNeedClientAuth)
+  }
+
+  "negotiableSession" must "have ClientAuth defined Need" in {
+    assert(negotiableSession.clientAuth.contains(TLSClientAuth.Need))
+  }
+
+  "SSLEngine" must "have ClientAuth defined Need after calling akka.stream.impl.io.TlsUtils.applySessionParameters with sslEngine and negotiableSession" in {
+    TlsUtils.applySessionParameters(sslEngine, negotiableSession)
+    assert(sslEngine.getSSLParameters.getNeedClientAuth)
+  }
+
+}
