b9aede06bbc8f09f887179cb5ffd72b02b991f94
economist <tigranohanyan777@gmail.com>
2020-09-26 21:26:58 +0400

    Pass PR

--- akka-stream-tests/src/test/scala/akka/stream/impl/io/TLSUtilsSpec.scala
+++ akka-stream-tests/src/test/scala/akka/stream/impl/io/TLSUtilsSpec.scala
@@ -1,16 +1,20 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
 package akka.stream.impl.io
 
 import akka.stream.TLSClientAuth
 import akka.stream.TLSProtocol.NegotiateNewSession
-import javax.net.ssl.{SSLContext, SSLEngine, SSLParameters}
+import javax.net.ssl.{ SSLContext, SSLEngine, SSLParameters }
 import org.scalatest.flatspec.AnyFlatSpec
-
 
 class TLSUtilsSpec extends AnyFlatSpec {
 
   val sslEngine: SSLEngine = SSLContext.getDefault.createSSLEngine()
   val sslParams: SSLParameters = new SSLParameters()
-  val negotiableSession: NegotiateNewSession = NegotiateNewSession(None,None,Some(TLSClientAuth.Need),Some(sslParams))
+  val negotiableSession: NegotiateNewSession =
+    NegotiateNewSession(None, None, Some(TLSClientAuth.Need), Some(sslParams))
 
   "SSLParameters" must "not by default require client authentication when it is not passed to the constructor of HttpConnectionContext. In that case its default value is used by akka-http." in {
     assert(!sslParams.getNeedClientAuth)
